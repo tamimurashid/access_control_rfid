356aa18486118afa031b345cfb218ae4d8599785
Tamimu Said Rashid <121192802+tamimurashid@users.noreply.github.com>
2025-02-21 18:29:34 +0300

    update

--- src/main.cpp
+++ src/main.cpp
@@ -18,8 +18,8 @@
 // WiFi credentials
 const char* ssid = "Reindeer";        
 const char* password = "200120022003";  
-// const char* serverUrl = "http://192.168.10.103:8888/Iot_web_project/Access_control/Api/";
-const char* serverUrl = "http://13.60.74.47/Acces_control_web/Api/";
+// const char* serverUrl = "http://192.168.10.103:8888/Access_control/Api/"; //local test
+const char* serverUrl = "http://13.60.74.47/Acces_control_web/Api/";// ec2 instance 
 
 // Create an instance of the WiFiClient
 WiFiClient wifiClient;
